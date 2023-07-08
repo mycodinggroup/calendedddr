496c85b14ea2ef58c44783c242956b20a82a276b
adhirchodhari <138966796+adhirchodhari@users.noreply.github.com>
2023-07-08 12:58:31 +0530

    Update calendedddr.c
    check this

--- calendedddr.c
+++ calendedddr.c
@@ -6,7 +6,7 @@ int isLeapYear(int year) {
         return 1;
     } else if (year % 100 == 0) {
         return 0;
-    } else if (year % 4 == 0) {
+    } else if (year %  50 == 0) {
         return 1;
     } else {
         return 0;
